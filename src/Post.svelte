ca7a683301b327019b4db3445ee83f17fe34ac48
iddev5 <ayushbardhan5@gmail.com>
2023-06-23 18:33:32 +0530

    Post: add additional information about the post

--- src/Post.svelte
+++ src/Post.svelte
@@ -9,7 +9,7 @@
     onMount(async () => {
         post = await pb.collection("posts").getOne(id, {
             expand: "user",
-            fields: "id,title,content,user",
+            fields: "id,title,content,user,created",
         });
         post.expand = {
             user: await pb.collection("publicusers").getOne(post.user),
@@ -36,6 +36,27 @@
         newCommentText = "";
         comments = [...comments, comment];
     };
+
+    function timeAgo(input) {
+        const date = input instanceof Date ? input : new Date(input);
+        const formatter = new Intl.RelativeTimeFormat("en");
+        const ranges = {
+            years: 3600 * 24 * 365,
+            months: 3600 * 24 * 30,
+            weeks: 3600 * 24 * 7,
+            days: 3600 * 24,
+            hours: 3600,
+            minutes: 60,
+            seconds: 1,
+        };
+        const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+        for (let key in ranges) {
+            if (ranges[key] < Math.abs(secondsElapsed)) {
+                const delta = secondsElapsed / ranges[key];
+                return formatter.format(Math.round(delta), key);
+            }
+        }
+    }
 </script>
 
 {#if post}
@@ -54,13 +75,24 @@
             <h4 class="title is-3">
                 {post.title}
             </h4>
-            <p>
-                (<a href={post.content}> {new URL(post.content).hostname}</a>)
-            </p>
+            <div class="columns">
+                <div class="column is-narrow">
+                    <p>
+                        (<a href={post.content}>
+                            {new URL(post.content).hostname}</a
+                        >)
+                    </p>
+                </div>
+                <div class="column is-narrow">{comments.length} comments</div>
+                <div class="column is-narrow">
+                    Posted {timeAgo(post.created)}
+                </div>
+            </div>
         </div>
         <div class="column">
-            <span class="tag is-primary">
-                {post.expand.user?.username ?? " "}
+            <span class="tag is-primary mt-2">
+                <!-- TODO: clickable -->
+                <a>{post.expand.user?.username ?? " "}</a>
             </span>
         </div>
     </div>
